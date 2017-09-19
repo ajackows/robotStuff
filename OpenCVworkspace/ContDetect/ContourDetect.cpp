06c8c04ce64879ab2bfbe4ffd89e74dc6f9d82d0
ajackows <atj236@nyu.edu>
2017-09-19 13:47:17 -0400

    added modified contour detect code

--- OpenCVworkspace/ContDetect/ContourDetect.cpp
+++ OpenCVworkspace/ContDetect/ContourDetect.cpp
@@ -16,59 +16,70 @@ RNG rng(12345);
 void thresh_callback(int, void* );
 
 /** @function main */
-int main( int argc, char** argv )
+int main( int argc, char** argv)
 {
 
 	VideoCapture cap(0); //capture the video from webcam
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
-    }
-	
+	if ( !cap.isOpened() )  // if not success, exit program
+	{
+	    cout << "Cannot open the web cam" << endl;
+	    return -1;
+	}
+	  
 
-	/// Create Window
-	char* source_window = "Source";
-	namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-	
-	
-	src = imread( argv[1], 1 );
-	
-	/// Convert image to gray and blur it
-	cvtColor( src, src_gray, CV_BGR2GRAY );
-	blur( src_gray, src_gray, Size(3,3) );
-	
-	imshow( source_window, src );
+	while(true)
+	{ 
+		//src = imread( argv[1], 1 );
+			  
+		bool bSuccess = cap.read(src);
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
 
-	createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
-	thresh_callback( 0, 0 );
+		// Convert image to gray and blur it
+		cvtColor( src, src_gray, CV_BGR2GRAY );
+		blur( src_gray, src_gray, Size(3,3) );
+		  
+		// Create Window
+		//char* source_window = "Source";
+		namedWindow( "Source", CV_WINDOW_AUTOSIZE);
+		imshow( "Source", src );
 
-	waitKey(0);
-	return(0);
+		createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
+		thresh_callback( 0, 0 );
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break; 
+		}
+		
+	}
+	return 0;
 }
 
-/** @function thresh_callback */
+	/** @function thresh_callback */
 void thresh_callback(int, void* )
 {
-	Mat canny_output;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-
-	/// Detect edges using canny
-	Canny( src_gray, canny_output, thresh, thresh*2, 3 );
-	/// Find contours
-	findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-	/// Draw contours
-	Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-	for( int i = 0; i< contours.size(); i++ )
-	{
+  Mat canny_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+  /// Detect edges using canny
+  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
+  /// Find contours
+  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  /// Draw contours
+  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+  for( int i = 0; i< contours.size(); i++ )
+  {
        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-     }
+  }
 
-	/// Show in a window
-	namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-	imshow( "Contours", drawing );
+  /// Show in a window
+  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+  imshow( "Contours", drawing );
 }
